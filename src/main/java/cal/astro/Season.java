93ee18a6699646b1471e74d20c456bcc07b8758c
Chris Engelsma <chris.engelsma@gmail.com>
2015-08-06 15:40:10 -0500

    Update Season.java
    Added comments

--- src/main/java/cal/astro/Season.java
+++ src/main/java/cal/astro/Season.java
@@ -1,10 +1,14 @@
+/**
+ * The four seasons, marked by their corresponding equinoxes/solstices.
+ */
+
 package cal.astro;
 
 public enum Season {
-  SPRING(0),
-  SUMMER(1),
-  AUTUMN(2),
-  WINTER(3);
+  SPRING(0), // Spring equinox
+  SUMMER(1), // Summer solstice
+  AUTUMN(2), // Autumnal equinox
+  WINTER(3); // Winter solstice
 
   private final int id;
   Season(int id) { this.id = id; }
