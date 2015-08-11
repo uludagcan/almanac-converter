f8d85f9b3a2635442f85e4d7cd8115ffd3aee6ac
Chris Engelsma <chris.engelsma@gmail.com>
2015-08-11 14:21:49 -0500

    Create Converter.java

---
+++ src/main/java/cal/util/Converter.java
@@ -0,0 +1,14 @@
+package cal.util;
+
+import cal.date;
+import cal.astro;
+
+/**
+ * A conversion facilitator between different calendars.
+ * @author Chris Engelsma
+ * @version 1.0
+ * @since 2015-08-11
+ */
+public class Converter() {
+  
+}
