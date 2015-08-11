34559b3cf5705fec3ae1116b1c97c80ed7762d29
Chris Engelsma <chris.engelsma@gmail.com>
2015-08-11 14:57:31 -0500

    Update Converter.java

--- src/main/java/cal/util/Converter.java
+++ src/main/java/cal/util/Converter.java
@@ -10,7 +10,38 @@ import cal.astro.*;
  * @since 2015-08-11
  */
 public class Converter {
-  public static Almanac convert(Almanac cal) { 
-    return cal;
+  public static JulianDay convert(Almanac cal) { 
+    if (cal instanceOf GregorianDate)
+      return _JD2GD(cal);
+   return (JulianDay)cal;
+  }
+  
+  public static GregorianDay convert(Almanac cal) {
+    
+  }
+  
+  public static FrenchRepublicanDate convert(Almanac cal) {
+    
+  }
+
+
+/////////////////////////////////////////////////////////////////////////////
+// private
+
+  private static JulianDay _JD2GD(GregorianDate date) {
+    int month = date.month;
+    int year = date.year;
+    int day = date.day;
+    if (month==1 || month==2) {
+      year--;
+      month+=12;
+    }
+    int a = (int)Math.floor(year/100);
+    int b = a/4;
+    int c = 2-a+b;
+    int e = (int)(365.25*(year+4716));
+    int f = (int)(30.6001*(month+1));
+    double JDN = c+day+e+f-1524.5;
+    return new JulianDay(JDN);
   }
 }
