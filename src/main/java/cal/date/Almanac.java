e03d65192424178afe1ce531efffb90e7d1da677
Chris Engelsma <chris.engelsma@gmail.com>
2015-08-11 14:28:24 -0500

    Update Almanac.java

--- src/main/java/cal/date/Almanac.java
+++ src/main/java/cal/date/Almanac.java
@@ -1,10 +1,11 @@
 package cal.date;
 
 /**
- * A generic almanac.
+ * A generic almanac interface.
  * @author Chris Engelsma
  * @version 1.0
  * @since 2015-08-11
  */
 public interface Almanac {
+  public void print();
 }
