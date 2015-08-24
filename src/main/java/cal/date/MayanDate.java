1a35eb916ed52581e0697a58e7f0f62026f7ba7f
Chris Engelsma <chris@hypotemoose.com>
2015-08-24 15:36:52 -0500

    Update MayanDate.java

--- src/main/java/cal/date/MayanDate.java
+++ src/main/java/cal/date/MayanDate.java
@@ -11,18 +11,28 @@ import cal.astro.*;
  */ 
 public class MayanDate implements Almanac {
   public static final String CALENDAR_NAME = "Mayan Calendar";
+  public static final float EPOCH = 584282.5f;
   
   public MayanDate() {
     this(new JulianDay());
   }
   
   public MayanDate(JulianDay jday) {
-    // TODO
+    _julianDayToMayanCount(jday);
   }
   
+  /**
+   * Gets the "Long Count" Mayan Date.
+   * @returns The long count mayan date.
+   */
   @Override
   public String getDate() {
-    return "Not yet implemented";
+    return new String(_baktun+"."+
+                      _katun+"."+
+                      _tun+"."+
+                      _winal+"."+
+                      _kin);
+
   }
   
   @Override
@@ -33,13 +43,39 @@ public class MayanDate implements Almanac {
 /////////////////////////////////////////////////////////////////////////////
 // private
 
-  private int _kin;
-  private int _uinal;
-  private int _tun;
-  private int _katun;
-  private int _baktun;
-  private int _pictun;
-  private int _calabtun;
-  private int _kinchiltun;
-  private int _alautun;
+  private void _julianDayToMayanCount(JulianDay jday) {
+    float day = (jday.setToNoon()).getDay();
+    float d = day - EPOCH;
+    _baktun = Math.floor(d/144000);
+    d = d % 144000;
+    _katun = Math.floor(d/7200);
+    d = d % 7200;
+    _tun = Math.floor(d / 360);
+    d = d % 360;
+    _winal = Math.floor(d / 20);
+    _kin = d % 20;
+  }
+
+  /**
+   * Conversion between Mayan count units are as follows:
+   * Long Count Unit  |  Long Count Period  | Days
+   * 1 K'in           |                     | 1
+   * 1 Winal          | 20 K'in             | 20
+   * 1 Tun            | 18 Winal            | 360
+   * 1 K'atun         | 20 Tun              | 7,200
+   * 1 B'ak'tun       | 20 K'atun           | 144,000
+   * 1 Piktun         | 20 B'ak'tun         | 2,880,000
+   * 1 Kalabtun       | 20 Piktun           | 57,600,000
+   * 1 K'inchiltun    | 20 Kalabtun         | 1,152,000,000
+   * 1 Alautun        | 20 K'inchiltun      | 23,040,000,000
+   */ 
+  private int _kin,        _lkin = 1;
+  private int _winal,      _lwinal = 20;
+  private int _tun,        _ltun = 360;
+  private int _katun,      _lkatun = 7200;
+  private int _baktun,     _lbaktun = 144000;
+  private int _piktun,     _lpiktun = 2880000;
+  private int _kalabtun,   _lkalabtun = 57600000;
+  private int _kinchiltun, _lkinchiltun = 1152000000;
+  private int _alautun,    _lalautun = 23040000000;
 }
