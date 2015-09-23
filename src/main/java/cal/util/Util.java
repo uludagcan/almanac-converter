ed5e6d0593cef94238edc3ae882262f4a5898e04
Chris Engelsma <chris@hypotemoose.com>
2015-09-23 16:30:40 -0500

    Update Util.java

--- src/main/java/cal/util/Util.java
+++ src/main/java/cal/util/Util.java
@@ -7,4 +7,28 @@ public final class Util {
   public static String its(int val) {
     return Integer.toString(val);
   }
+
+  public static double dsin(double deg) {
+    return Math.sin(dtr(deg));
+  }
+
+  public static double dcos(double deg) {
+    return Math.cos(dtr(deg));
+  }
+
+  public static double rtd(double rad) {
+    return Math.toDegrees(rad);
+  }
+
+  public static double dtr(double deg) {
+    return Math.toRadians(deg);
+  }
+
+  public static double fixAngle(double a) {
+    return (a - 360.0 * (Math.floor(a/360.0)));
+  }
+
+  public static double fixAngler(double rad) {
+    return rad - (2*Math.PI)*(Math.floor(rad/(2*Math.PI)));
+  }
 }
