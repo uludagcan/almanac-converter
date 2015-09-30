a26eb0be44ddbd99add76b22093cc229175c86b1
Chris Engelsma <chris@hypotemoose.com>
2015-09-30 15:12:17 -0500

    Update RomanNumeralGenerator.java

--- src/main/java/cal/util/RomanNumeralGenerator.java
+++ src/main/java/cal/util/RomanNumeralGenerator.java
@@ -8,7 +8,7 @@ package cal.util;
  * the 14th century, Roman numerals began to be replaced in most contexts by
  * Arabic numbers.
  * @author Chris Engelsma
- * @version 2015-09-22
+ * @version 2015.09.22
  */
 public final class RomanNumeralGenerator {
 
